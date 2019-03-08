5d7779e45fafc5b7c1827d00b99f95618feaa42b
Jennifer Powell <jerpowel321@gmail.com>
2019-03-07 23:26:17 -0800

    updated for moments

--- train_scheduler.js
+++ train_scheduler.js
@@ -7,101 +7,124 @@
 //    Then use moment.js formatting to set difference in months.
 // 5. Calculate Total billed
 
-// 1. Initialize Firebase
+// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBcu4ExkbdcNkrZuWHB-p9AjKodYgJRy84",
-    authDomain: "train-scheduler-5747d.firebaseapp.com",
-    databaseURL: "https://train-scheduler-5747d.firebaseio.com",
-    projectId: "train-scheduler-5747d",
-    storageBucket: "",
-    messagingSenderId: "614037026238"
+    apiKey: "AIzaSyAVqZ7-PMQTUmbxQNR18DiA105jJJ4U8ys",
+    authDomain: "train-scheduler-1317b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1317b.firebaseio.com",
+    projectId: "train-scheduler-1317b",
+    storageBucket: "train-scheduler-1317b.appspot.com",
+    messagingSenderId: "201832885897"
   };
   firebase.initializeApp(config);
-  
-  var database = firebase.database();
-  
-  // 2. Button for adding Employees
-  $("#add-train-btn").on("click", function(event) {
+
+// Variable to reference the database
+var database = firebase.database();
+
+// Initial Values
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+var nextArrival = "HH/MM/YYYY";
+var minutesAway = 0;
+
+// Button for adding Trains
+$("#add-train-btn").on("click", function (event) {
     event.preventDefault();
-  
+
     // Grabs user input
-    var trainName = $("#train-name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var firstTrainTime = moment($("#firstTrainTime-input").val().trim(), "MM/DD/YYYY").format("X");
-    var frequency = $("#frequency-input").val().trim();
-  
-    // Creates local "temporary" object for holding employee data
+    trainName = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrainTime = moment($("#firstTrainTime-input").val().trim(), "HH/MM/").format("X");
+    frequency = $("#frequency-input").val().trim();
+
+    // Creates object for holding train data
     var newTrain = {
-      trainName: trainName,
-      destination: destination,
-      firstTrainTime: firstTrainTime,
-      frequency: frequency
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency,
+        nextArrival: nextArrival,
+        minutesAway: minutesAway,
     };
-  
-    // Uploads employee data to the database
+
+    // Uploads train data to the database
     database.ref().push(newTrain);
-  
+
     // Logs everything to console
     console.log(newTrain.trainName);
     console.log(newTrain.destination);
     console.log(newTrain.firstTrainTime);
     console.log(newTrain.frequency);
-  
-    alert("Employee successfully added");
-  
+
+    // alert("Train successfully added");
+
     // Clears all of the text-boxes
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#firstTrainTime-input").val("");
     $("#frequency-input").val("");
-  });
-  
-  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-  database.ref().on("child_added", function(childSnapshot) {
+});
+
+var count = 1;
+
+// Firebase watcher .on("child_added" and add a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-  
+
     // Store everything into a variable.
     var trainName = childSnapshot.val().trainName;
     var destination = childSnapshot.val().destination;
     var firstTrainTime = childSnapshot.val().firstTrainTime;
     var frequency = childSnapshot.val().frequency;
-  
-    // Employee Info
+
+    // Train Info
     console.log(trainName);
     console.log(destination);
     console.log(firstTrainTime);
     console.log(frequency);
-  
-    // Prettify the employee start
-    var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
-  
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    var empMonths = moment().diff(moment(empStart, "X"), "months");
-    console.log(empMonths);
-  
-    // Calculate the total billed rate
-    var empBilled = empMonths * empRate;
-    console.log(empBilled);
-  
+
+    // Prettify the Train start time
+    // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+    
+    var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+    
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    var minutesAway = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " +minutesAway);
+
+
+    var nextArrivals = moment().add(minutesAway, "minutes");
+    var nextArrival = moment(nextArrivals).format("hh:mm");
+
     // Create the new row
     var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(destination),
-      $("<td>").text(frequency),
-      $("<td>").text(nextArrival),
-      $("<td>").text(minutesAway),
-    
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesAway),
+
     );
-  
+
     // Append the new row to the table
-    $("#train-table > tbody").append(newRow);
-  });
-  
+    $("#train-table").append(newRow);
+});
+
   // Example Time Math
   // -----------------------------------------------------------------------------
   // Assume Employee start date of January 1, 2015
   // Assume current date is March 1, 2016
-  
+
   // We know that this is 15 months.
   // Now we will create code in moment.js to confirm that any attempt we use meets this test case
